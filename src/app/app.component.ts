0a5dad295df26c80da8047df924a88932195980f
Etienne D'Aragon <daragon@gmail.com>
2017-08-01 00:22:52 -0400

    Adding the coveoService + displaying simple result

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/distinctUntilChanged";
+import "rxjs/add/operator/switchMap";
+
+import { CoveoConnectorService } from './service/coveo-connector.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent /*implements OnInit*/{
   title = 'Boozle';
+  private searchText = new Subject<String>();
+  private resultList: any[];
 
-  search(searchText: String){
-    console.log(searchText);
+
+  constructor(private coveoConnectorService:CoveoConnectorService){}
+
+  ngOnInit() {
+    this.searchText
+      .debounceTime(100) 
+      .distinctUntilChanged()
+      .switchMap(inputText => this.coveoConnectorService.search(inputText, "058c85fd-3c79-42a3-9236-b83d35588103"))
+      .subscribe((vals) => {
+        this.resultList = vals;
+      });
+  }
+
+  search(imputText: String){
+    this.searchText.next(imputText);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }      from '@angular/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { CoveoConnectorService } from './service/coveo-connector.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [CoveoConnectorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
