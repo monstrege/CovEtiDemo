c1f00f50ba3d56d8fded5e828a694a434c7e7179
Etienne D'Aragon <daragon@gmail.com>
2017-08-07 02:49:08 -0400

    Not final, but that's what I have for the presentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule }      from '@angular/http';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MainComponent } from './view/mainPage.component';
 
 import { CoveoConnectorService } from './service/coveo-connector.service';
 import { LoadMaskDirective } from './directive/loadmask.directive';
 
 import { IonRangeSliderModule } from "ng2-ion-range-slider";
 
+const appRoutes: Routes = [{
+    path: 'searchText/:searchText/page/:page/sortIndex/:sortIndex', component: MainComponent
+  },{
+     path: '**', redirectTo: '', component: MainComponent
+  }];
+
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
     LoadMaskDirective
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    IonRangeSliderModule
+    IonRangeSliderModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [CoveoConnectorService],
   bootstrap: [AppComponent]
--- src/app/service/coveo-connector.service.ts
+++ src/app/service/coveo-connector.service.ts
@@ -4,24 +4,33 @@ import 'rxjs/add/operator/map'
 import 'rxjs/add/observable/of'
 import {Observable} from 'rxjs/Observable';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class CoveoConnectorService {
   private url:String = "https://cloudplatform.coveo.com/rest/";
-  private lastBoozeName: String;
   private lastToken: String;
   private sort:number;
   public loading = new BehaviorSubject<boolean>(false);
+  public results: BehaviorSubject<any[]> = new BehaviorSubject(null);
+  public resultMin: BehaviorSubject<number> = new BehaviorSubject(null);
+  public resultMax: BehaviorSubject<number> = new BehaviorSubject(null);
+  public pageSize = 10;
+  public activePage: BehaviorSubject<number> = new BehaviorSubject(1);
+  public totalCount: Subject<number> = new Subject();
+  private getSubscription;
+
+
 
   private searchParam = {
-     "numberOfResults": 20,
+     "numberOfResults": this.pageSize,
      "sortCriteria": ""
-    }
+  }
 
+  public searchText: string;
   public activeSortIndex: BehaviorSubject<number> = new BehaviorSubject(0);
-  public prizeMin: BehaviorSubject<number> = new BehaviorSubject(null);
-  public prizeMax: BehaviorSubject<number> = new BehaviorSubject(null);
-
+  public prizeMin: number;
+  public prizeMax: number;
 
   private _sortOptions: any[] = [{
     fr: 'Pertinence',
@@ -43,19 +52,28 @@ export class CoveoConnectorService {
   
   constructor(private http: Http) { }
 
-  search(boozeName: String, token:String){
+  ngOnDestroy(){
+    if(this.getSubscription && this.getSubscription.unsubscribe){
+      this.getSubscription.unsubscribe();
+    }
+  }
+
+  public search(){
     this.loading.next(true);
     const queryIdentifier: String = 'search';
 
-    this.lastBoozeName = boozeName;
-    this.lastToken= token;
-
-    return this.http
-          .get(this.queryBuilder(queryIdentifier, token) + boozeName, {params: this.searchParam})
-          .map((r: Response) => {
-            this.loading.next(false);
-            return r.json().results
-          });
+    this.getSubscription = this.http
+      .get(this.queryBuilder(queryIdentifier, "058c85fd-3c79-42a3-9236-b83d35588103"), {params: this.searchParam})
+      .map((r: Response) => r.json())
+      .subscribe(response=> {
+        this.loading.next(false);
+        this.totalCount.next(response.totalCount);
+        if(!this.prizeMin && !this.prizeMax){
+          this.resultMin.next(Math.min.apply(Math,response.results.map(function(o){return o.raw.tpprixnum || 0;})));
+          this.resultMax.next(Math.max.apply(Math,response.results.map(function(o){return o.raw.tpprixnum || 0;})));
+        }
+        this.results.next(response.results)
+      });
   }
 
   private queryBuilder(queryIdentifier: String, token:String){
@@ -64,27 +82,63 @@ export class CoveoConnectorService {
       queryIdentifier,
       '?',
       "access_token=" + token,
-      '&q='
+      '&q=' + this.searchText,
+      this.prizeMax ? " @tpprixnum<=" + this.prizeMax: '',
+      this.prizeMin ? " @tpprixnum>=" + this.prizeMin: '',
     ];
     return queryItems.join('');
+  }
+
+  setSearchText(searchText: string){
+    if(searchText === this.searchText){
+      return;
+    }
+    this.searchText = searchText;
+    this.prizeMin = null;
+    this.prizeMax = null;
   }
 
   setSort(sortIndex: number){
     if(sortIndex === this.activeSortIndex.getValue()){
       return;
     }
+
+    sortIndex = sortIndex || 0;
+
     this.activeSortIndex.next(sortIndex);
+    this.prizeMin = null;
+    this.prizeMax = null;
 
     this.searchParam.sortCriteria = !!this.sortOptions[sortIndex].key ? "@tpprixnum " + this.sortOptions[sortIndex].key : "";
   }
 
-  // setPrizeRange(min: number, max: number){
-  //   if(min === this.prizeMin.getValue() && max === this.prizeMax.getValue()){
-  //     return;
-  //   }
+  setPrizeRange(min: number, max: number){
+    if(min === this.prizeMin && max === this.prizeMax){
+      return;
+    }
 
-  //   this.activeSortIndex.next(sortIndex);
+    this.prizeMin = min;
+    this.prizeMax = max;
 
-  //   this.searchParam.sortCriteria = !!this.sortOptions[sortIndex].key ? "@tpprixnum " + this.sortOptions[sortIndex].key : "";
-  // }
+    if(this.resultMin.getValue() === min){
+      this.prizeMin = null;
+    }
+    if(this.resultMax.getValue() === max){
+      this.prizeMax = null;
+    }
+    this.search();
+  }
+
+  setPage(page: number){
+    if(page === this.activePage.getValue()){
+      return;
+    }
+
+    if(page){
+      this.activePage.next(page);
+      this.searchParam['firstResult'] = (this.activePage.getValue() -1) * this.pageSize;
+    } else {
+      delete this.searchParam['firstResult'];
+    }
+  }
 }
